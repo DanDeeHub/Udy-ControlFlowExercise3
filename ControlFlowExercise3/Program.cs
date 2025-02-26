7801be17d6949e00b3717f6f0989baa5058fbe37
Dan <danangelodemontano13@gmail.com>
2025-02-26 17:16:10 +0800

    Add project files.

---
+++ ControlFlowExercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ControlFlowExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: "); // 5
+            var number = Convert.ToInt32(Console.ReadLine()); // 5
+            var factorial = 1; // 1
+            for (var i = 1; i <= number; i++) // 1, 2, 3, 4, 5
+            {
+                factorial *= i; // 1, 2, 6, 24, 120
+            }
+            Console.WriteLine("{0}! = {1}", number, factorial); // 5! = 120
+        }
+    }
+}
